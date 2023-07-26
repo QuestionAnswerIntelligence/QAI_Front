8246c29f1f32dd92bd7a66b0dffe053b90316568
AhnRian <00rian@naver.com>
2023-07-26 19:06:24 +0900

    [add]메인페이지 기본 디자인

---
+++ cafeflow/src/components/Chat/Chat.js
@@ -0,0 +1,14 @@
+
+import React from "react";
+import  "./Chat.css";
+
+const Chat=()=>{
+    return(
+        <div className="chat">
+            <button className="chatButton"><span>채팅</span></button>
+        </div>
+    )
+
+}
+
+export default Chat;
--- cafeflow/src/components/Home/Home.js
+++ cafeflow/src/components/Home/Home.js
@@ -2,15 +2,28 @@ import React from "react";
 
 import Header from "../Layout/header/Header";
 import Footer from "../Layout/footer/Footer";
+import Chat from "../Chat/Chat"
 
 import "./Home.css";
+
 
 const Home = () => {
   return (
     <div>
       <Header />
+      <Chat/>
+
       <div className="main">
         <h2>메인 홈페이지</h2>
+      </div>
+      <div className="aa">
+          
+      </div>
+      <div className="aa">
+          
+      </div>
+      <div className="aa">
+          
       </div>
       <Footer />
     </div>
